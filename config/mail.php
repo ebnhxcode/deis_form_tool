30eca80d98ed77adaf0e1b3c25a578ddafbd74fb
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2017-11-02 19:41:27 -0300

    update/add - add config for send email.

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+   'from' => ['address' => 'sistemas@minsal.cl', 'name' => 'Sistemas'],
 
     /*
     |--------------------------------------------------------------------------
