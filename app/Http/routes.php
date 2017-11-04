cd07240f9ac10041613bc4db1faaad5b1f0faee1
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2017-11-04 12:53:39 -0300

    update/add - backing up.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,6 +13,10 @@
 
 #Route::get('/', function () {return view('welcome');});
 
+Route::get('/demo', function () {
+   return view('demo.demo');
+});
+
 Route::get('/registro', 'UserController@registro');
 Route::get('/solicitud_clave', 'UserController@registro');
 Route::get('/clave_electronica', 'UserController@registro');
